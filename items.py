bbfd601c439e8353de8686520200d310af2f4adf
André Flemming <andre.flemming@kape.com>
2021-12-23 10:10:47 +0100

    Fixed: symlink no exist; not enough values to unpack

--- items.py
+++ items.py
@@ -1,8 +1,8 @@
 symlinks = {}
 directories = {}
 
-for path, config in node.metadata.get('symlinks', {}):
-    symlink[path] = {
+for path, config in node.metadata.get('symlinks', {}).items():
+    symlinks[path] = {
         'target': config.get('target'),
         'owner': config.get('owner', 'root'),
         'group': config.get('group', 'root'),
